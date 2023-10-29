70659044e858f8db67887e34ed2024794e590127
Mads <Mads@LAPTOP-6FUP8OA4>
2023-10-29 11:36:09 +0100

    Added RoamActivity

---
+++ Assets/Scripts/AI/FSM/Activity/RoamActivity.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using AI.FSM;
+using UnityEngine;
+
+public class RoamActivity : Activity
+{
+    public override void Enter(BaseStateMachine stateMachine)
+    {
+        stateMachine.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        stateMachine.GetComponent<Animator>().SetFloat("moveSpeed", 0);
+        // TODO: Movespeed float should probably be changed to a boolean, as the value of movespeed has no effect other than if it is above 0 or not.
+    }
+
+    public override void Execute(BaseStateMachine stateMachine)
+    {
+    }
+
+    public override void Exit(BaseStateMachine stateMachine)
+    {
+    }
+}

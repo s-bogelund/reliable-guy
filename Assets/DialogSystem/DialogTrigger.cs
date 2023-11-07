9d9e66903b5c19ec87304fec88cf0386ceeda6ab
oscarboe <Oscarboen@gmail.com>
2023-11-07 14:31:35 +0100

    Added tutorial

--- Assets/DialogSystem/DialogTrigger.cs
+++ Assets/DialogSystem/DialogTrigger.cs
@@ -6,44 +6,35 @@ using UnityEngine;
 
 public class DialogTrigger : MonoBehaviour
 {
-    public Dialog dialog;
+    public Dialog[] Dialogs;
 
     public void TriggerDialog()
     {
-        FindObjectOfType<DialogManager>().StartDialog(dialog);
+        foreach (Dialog dialog in Dialogs)
+        {
+            FindObjectOfType<DialogManager>().StartDialog(dialog);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Debug.Log("Triggered");
         if (collision != null)
         {
-            Dialog dialog = new Dialog("Player", new string[] {
-                "I found some dirt!",
-                "I should bring it to the scientist."
-            });
+        
             if (collision.CompareTag("Player"))
             {
-                Debug.Log(dialog.name);
-                FindObjectOfType<DialogManager>().StartDialog(dialog);
+                Debug.Log(Dialogs[0].name);
+                foreach (Dialog dialog in Dialogs)
+                {
+                    FindObjectOfType<DialogManager>().StartDialog(dialog);
+                }
                 Destroy(gameObject);
             }
         }
-
         else
         {
             Debug.LogError("Collision object is null. Make sure it's properly initialized.");
         }
     }
-
-    //triggers dialog with the sentences between sentenceIndexStart, and sentenceIndexEnd
-    public void TriggerDialog(int sentenceIndexStart,int sentenceIndexEnd)
-    {
-        var tempDialog = new Dialog();
-        var tempSentences = dialog.sentences.Skip(sentenceIndexStart).ToArray();
-        tempDialog.name = dialog.name;
-        tempDialog.sentences = dialog.sentences.Take(sentenceIndexEnd-sentenceIndexStart).ToArray();
-        FindObjectOfType<DialogManager>().StartDialog(tempDialog);
-    }
-   
- 
 }

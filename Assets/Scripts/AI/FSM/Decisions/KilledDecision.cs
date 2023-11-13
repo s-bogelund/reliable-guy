c44d41c4a29c7b0d2bd8a1a7b7e358304757af48
Mads <Mads@LAPTOP-6FUP8OA4>
2023-11-13 21:14:35 +0100

    Created scriptable objects

--- Assets/Scripts/AI/FSM/Decisions/KilledDecision.cs
+++ Assets/Scripts/AI/FSM/Decisions/KilledDecision.cs
@@ -1,5 +1,8 @@
+using UnityEngine;
+
 namespace AI.FSM.Decisions
 {
+    [CreateAssetMenu(menuName = "AI/FSM/Decisions/KilledDecision")]
     public class KilledDecision : Decision
     {
         public bool isDead = true;

07fb3687e6423784780ee3e1936c02c83ce535dd
s-bogelund <simbj94@gmail.com>
2023-12-18 17:22:46 +0100

    Updated town with narration

---
+++ Assets/Scenes/Scripts/EnableSwordAttackScript.cs
@@ -0,0 +1,57 @@
+using Controls;
+using UnityEngine;
+
+public class EnableSwordAttackScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float interactionDistance = 2f; // Distance within which interaction is possible
+    private PlayerController _playerController; // Reference to the PlayerController script
+    private SpriteRenderer _playerSpriteRenderer;
+    private InputHandler _inputHandler;
+
+    public GameObject player;
+    public ActionsCommandsScheme controls;
+
+    void Start()
+    {
+        // if(gameManager == null)
+        //     gameObject.GetComponent<GameManager>();
+        
+        player = GameObject.FindGameObjectWithTag("Player"); // Ensure your player GameObject has the "Player" tag
+        _playerController = player.GetComponent<PlayerController>();
+        _playerSpriteRenderer = player.GetComponent<SpriteRenderer>();
+        _inputHandler = player.GetComponent<InputHandler>();
+
+        
+        gameObject.SetActive(true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(transform.position, _playerController.transform.position) <= interactionDistance && IsPlayerFacingItem())
+        {
+            CheckForInteractionInput();
+        }
+    }
+    
+    void CheckForInteractionInput()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            InputHandler.instance.UpdateActionsCommandsList(controls.actionCommandList);
+            InputHandler.instance.UpdateActionsCommandsBindings();
+            gameObject.SetActive(false);
+            
+        }
+    }
+    bool IsPlayerFacingItem()
+    {
+        Vector3 directionToItem = (transform.position - _playerController.transform.position).normalized;
+        bool isFacingRight = !_playerSpriteRenderer.flipX;
+        float dotProduct = Vector3.Dot(isFacingRight ? Vector3.right : Vector3.left, directionToItem);
+        return dotProduct > 0;
+    }
+    
+}
